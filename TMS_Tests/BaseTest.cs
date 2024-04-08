1f279f12681be271effaab25be77860c077a9581
Andrei Kaliasinski <andykolesinski@gmail.com>
2024-04-08 07:56:17 +0300

    Added locators and sevraral tests

---
+++ TMS_Tests/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_Tests
+{
+    internal class BaseTest
+    {
+        protected IWebDriver Driver { get; set; }
+
+        [SetUp]
+        public void OneTimeSetup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--incognito");
+            Driver = new ChromeDriver(options);
+            Driver.Manage().Window.Maximize();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [TearDown]
+        public void OneTimeTearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
